90954a7797fea70e5cb93bce043869c3c6a43e47
arye321 <lionop@gmail.com>
2022-08-27 09:15:16 +0300

    +autofucus -darkmode fixed auto focus removed darkmode cuz of chrome bug improved error for link

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useRef, useEffect, useState } from 'react'
 import Results from '../components/Results'
 
 export default function Home() {
+
   //useRef
   // state info
   const [info, setInfo] = useState()
@@ -13,7 +14,10 @@ export default function Home() {
   const start_date = useRef(null)
   const end_date = useRef(null)
   const channel_link = useRef(null)
-
+  useEffect(() => {
+    // focust on input
+    channel_link.current.focus()
+  } , [])
   async function submited(e) {
     e.preventDefault()
     if (start_date.current.value && end_date.current.value && channel_link.current.value) {
@@ -21,8 +25,24 @@ export default function Home() {
       const start_date_value = start_date.current.valueAsDate.toISOString()
       const end_date_value = end_date.current.valueAsDate.toISOString()
       const channel_link_value = channel_link.current.value
-      // console.log(start_date_value)
-      setInfo({ channel_link: channel_link_value, start_date: start_date_value, end_date: end_date_value })
+      if (channel_link_value.includes('http')){
+      
+        // console.log(start_date_value)
+        if (channel_link_value.includes('/channel/')){
+          setInfo({ channel_link: channel_link_value, start_date: start_date_value, end_date: end_date_value })
+        }
+      }
+      else{
+        setError(<>
+        <p>Channel link must include http or https </p>
+        <p>Examples: </p>
+        <p>https://www.youtube.com/channel/UC-9-kyTW8Zk</p>
+        <p>https://www.youtube.com/c/DJMag/</p>
+        <p>https://www.youtube.com/user/PewDiePie</p>
+        </>)
+        
+
+      }
     }
     else {
       setError("Please fill out all fields")
@@ -42,18 +62,21 @@ export default function Home() {
       <input type="date" ref={end_date} id='dateEnd' />
       <br />
       <br />
-      <p>channel:</p>
+      <p>channel link:</p>
       <form onSubmit={submited}>
         <input
           className='channelLink'
           ref={channel_link}
           placeholder="https://www.youtube.com/channel/UCUujfNBK9uv3cIW-P5PX7vA"
-          autoFocus
+          autoFocus={true}
         />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
       </form>
       <div className='results'>
         {info ? <Results info={info} /> : null}
-        {error ? <div className="alert" role="alert">{error}</div> : null}
+        {error ? <div>{error}</div> : null}
 
       </div>
       {/* <h4>{JSON.stringify(info)}</h4> */}
